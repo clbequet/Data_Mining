e0874994a55083da08dfc5e69d43ac26f1543b6d
Clément <clement.bequet@hotmail.fr>
2023-09-13 14:40:15 +0200

    today 1309

---
+++ ge4.py
@@ -0,0 +1,24 @@
+def termes(n):
+    y = [1] * n
+    yield y
+    while y[0] != n-1:
+        if y[-1] <= 2:
+            y[-2] = y[-2] +1
+            y[-1] = y[-1] -1
+            if y[-1] == 0:
+                del y[-1]
+        else:
+            y[-2] = y[-2] +1
+            m = y[-1] -2
+            y[-1] = 1
+            for i in range(m):
+                y.append(1)
+        yield y
+
+suite = termes(7)
+k = 0
+for i in suite:
+    print(i)
+    if k == 80:
+        break
+    k +=1
